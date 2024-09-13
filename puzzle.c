0d9d5e47ca52e3d60fe48939dc04e411ba1d2b7e
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-13 22:54:46 +0530

    Add files via upload

---
+++ puzzle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int rows,i,j;
+    printf("enter the number of rows:");
+    scanf("%d",&rows);
+    for (i=1; i<=rows; ++i)
+    {
+        for (j=1; j<=i; ++j)
+        {
+            
+        }
+        printf("\n");
+    }
+    return 0;    
+}
+ 
