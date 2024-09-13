76303e8ece8ba2ec423af826a66600e60b9a4814
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-13 22:51:52 +0530

    Add files via upload

---
+++ choicefilling.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,y,n,x;
+    int result;
+    char ch;
+    printf("enter the value of a,b ");
+    scanf("%d%d",&a,&b);
+    printf("enter your operator");
+    switch(n)
+    {
+        case 1: 
+       result = a+b;
+        printf("the value of y is %f",y);
+        break;
+        case 2: y=a*x*x+b*b;
+        printf("the value of y is %f,y");
+        break;
+        case 3: y=a-b*x;
+        printf("the value of y is %f,y");
+        break;
+        case 4: y=a+ (float)x/b;
+        printf("the value of y is %f,y");
+        break;
+        default: printf("invalid choice");
+    }
+}
