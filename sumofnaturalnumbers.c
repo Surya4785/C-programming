7364d8dce7452f9634df9c3d2f28183f37e9db47
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-13 22:50:44 +0530

    Add files via upload

---
+++ sumofnaturalnumbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int sum,n;
+    printf("enter natural numbers");
+    scanf("%d%d",&n,&sum);
+    sum=n*(n+1)/2;
+    
+    printf("sum of natural numbers%d",sum); 
+
+}
