94e1edd0275789ca24dc2a38dfaebd1c27c3f7fb
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-10-08 23:47:09 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,18 @@
+/*C Program to Print Alphabets From A to Z Using Loop*/
+
+#include <stdio.h>
+int main() {
+
+ char ch;
+
+ printf("Alphabets from A to Z:\n");
+
+ for (ch = 'A'; ch <= 'Z'; ++ch) {
+
+ printf("%c", ch);
+
+ }
+
+ return 0;
+
+}
