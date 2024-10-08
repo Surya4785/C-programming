dd6854a504e2cfe2470a6f3f3b49c512688bed7a
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-10-08 23:33:19 +0530

    Add files via upload

---
+++ conversion.c
@@ -0,0 +1,19 @@
+/*C Program to Calculate Fahrenheit to Celsius*/
+
+#include <stdio.h>
+
+int main() {
+
+ float fahrenheit, celsius;
+
+ printf("Enter temperature in Fahrenheit:");
+
+ scanf("%f", &fahrenheit);
+
+ celsius = (fahrenheit - 32) * 5 / 9;
+
+ printf("Temperature in Celsius: %f\n", celsius);
+
+ return 0;
+
+}
