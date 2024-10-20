de808ba777d777a21b51f7045252bca11c315705
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-10-20 23:26:23 +0530

    Add files via upload

---
+++ pattern04.c
@@ -0,0 +1,15 @@
+// inverted right half pyramid of stars.
+
+#include <stdio.h>
+int main()
+{
+    int rows = 5;
+
+    for (int i = 0; i < rows; i++) {
+
+        for (int j = 0; j < rows - i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
